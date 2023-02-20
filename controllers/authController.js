8095443326aea4485e4c122b52d4d479e1b33d2b
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-02-20 16:27:29 +0530

    Error handlers added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,30 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from '../errors/index.js'
 
-const register = async (req, res) => {
-  res.send('User Sign UP successfully')
+const register = async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Provide all values");
+  }
+  const userAlreadyExists = await User.findOne({ email})
+  if(userAlreadyExists) {
+    throw new BadRequestError("Account with this email already exists");
+  }
+
+  res.status(StatusCodes.CREATED).json({ newUser });
 };
 
 const login = async (req, res) => {
-  res.send('User logged IN successfully')  
+  res.send("User logged IN successfully");
 };
 const updateUser = async (req, res) => {
-  res.send('User updated successfully')
+  res.send("User updated successfully");
 };
 
 export { register, login, updateUser };
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,23 +1,27 @@
-import { StatusCodes } from 'http-status-codes'
+import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  //This is a default error object that will be used if the error is not a validation error or a duplicate key error.
   const defaultError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || 'Something went wrong, try again later',
-  }
-  if (err.name === 'ValidationError') {
-    defaultError.statusCode = StatusCodes.BAD_REQUEST
+    msg: err.message || "Something went wrong, try again later",
+  };
+
+  //  If the error is a validation error, set the status code to BAD_REQUEST and set the  message to the error messages.
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
     // defaultError.msg = err.message
     defaultError.msg = Object.values(err.errors)
       .map((item) => item.message)
-      .join(',')
+      .join(",");
   }
+  // If the error is a duplicate key error, set the status code to BAD_REQUEST and set the error message to the name of the duplicate field.
   if (err.code && err.code === 11000) {
-    defaultError.statusCode = StatusCodes.BAD_REQUEST
-    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
   }
 
-  res.status(defaultError.statusCode).json({ msg: defaultError.msg })
-}
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+};
 
-export default errorHandlerMiddleware
+export default errorHandlerMiddleware;
