6a38dca97551c7742349d8c67b379c7ffa2be05d
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-02-21 17:06:18 +0530

    auth functionality implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,26 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-import jwt from 'jsonwebtoken';
-import { BadRequestError, NotFoundError, UnauthenticatedError } from "../errors/index.js";
-
+import jwt from "jsonwebtoken";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} from "../errors/index.js";
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    throw new BadRequestError('please provide all values');
+    throw new BadRequestError("please provide all values");
   }
   const userAlreadyExists = await User.findOne({ email });
   if (userAlreadyExists) {
-    throw new BadRequestError('Email already in use');
+    throw new BadRequestError("Email already in use");
   }
   const user = await User.create({ name, email, password });
 
   const token = user.createJWT();
-  console.log('token:', token);
+  console.log("token:", token);
   // attachCookie({ res, token });
   res.status(StatusCodes.CREATED).json({
     user: {
@@ -25,9 +28,8 @@ const register = async (req, res) => {
       lastName: user.lastName,
       location: user.location,
       name: user.name,
-      token,
     },
-
+    token,
     location: user.location,
   });
 };
@@ -50,28 +52,28 @@ const register = async (req, res) => {
 //   res.status(StatusCodes.CREATED).json({ newUser });
 // };
 
-
 // We're finding a user by email, selecting the password field, comparing the password, and then creating a JWT token.
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    throw new BadRequestError('Please provide all values');
+    throw new BadRequestError("Please provide all values");
   }
   /* Finding a user by email and selecting the password field. We need to specifically select the password field because it has the 'select: false' in userSchema */
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
-    throw new UnauthenticatedError('Invalid Credentials');
+    throw new UnauthenticatedError("Invalid Credentials");
   }
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError('Invalid Credentials');
+    throw new UnauthenticatedError("Invalid Credentials");
   }
   const token = user.createJWT();
   user.password = undefined;
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 const updateUser = async (req, res) => {
+  console.log(req.user);
   res.send("User updated successfully");
 };
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,23 @@
+import jwt from "jsonwebtoken";
+import { UnauthenticatedError } from "../errors/index.js";
+
 const auth = async (req, res, next) => {
-    console.log('authenticate user');
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    // 401
+    throw new UnAuthenticatedError("Authentication Invalid");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(payload);
+    req.user = { userId: payload.userId };
     next();
-  };
-  
-  export default auth;
+  } catch (error) {
+    throw new UnAuthenticatedError("Authentication Invalid");
+  }
+
+  next();
+};
+
+export default auth;
