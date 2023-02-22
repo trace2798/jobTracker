0e0f4eb5bf68f1f04ea24d0a6fe82c84c8927203
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-02-22 14:52:40 +0530

    createJob server-side functionality added

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import { useState } from 'react';
+import { FormRow, Alert } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
-const AddJob = () => {
+const Profile = () => {
+  const { user, showAlert, displayAlert, updateUser, isLoading } =
+    useAppContext();
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [location, setLocation] = useState(user?.location);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email || !lastName || !location) {
+      displayAlert();
+      return;
+    }
+
+    updateUser({ name, email, lastName, location });
+  };
   return (
-    <div>AddJob</div>
-  )
-}
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>Add job </h3>
+        {showAlert && <Alert />}
 
-export default AddJob
+        {/* name */}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='Position'
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            labelText='Company'
+            type='text'
+            name='Company'
+            value={lastName}
+            handleChange={(e) => setLastName(e.target.value)}
+          />
+          
+          <FormRow
+            type='text'
+            name='location'
+            value={location}
+            handleChange={(e) => setLocation(e.target.value)}
+          />
+          <button className='btn btn-block' type='submit' disabled={isLoading}>
+            {isLoading ? 'Please Wait...' : 'save changes'}
+          </button>
+          <button className='btn btn-block' type='clear' disabled={isLoading}>
+            {isLoading ? 'Please Wait...' : 'Clear'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Profile;
--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,24 @@
 import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
 const createJob = async (req, res) => {
-  res.send(" Create Job");
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError("please provide all values");
+  }
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+  
 };
 
 const deleteJob = async (req, res) => {
   res.send(" Delete Job");
 };
+
 const getAllJobs = async (req, res) => {
   res.send(" getAllJobs Job");
 };
