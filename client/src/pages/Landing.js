fecf62d5f1a6650584ec600ab15d828c4ab97858
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-02-25 23:49:29 +0530

    package lock deleted

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -22,10 +22,7 @@ const Landing = () => {
               job <span>tracking</span> app
             </h1>
             <p>
-              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-              bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-              quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-              narwhal.
+              Keep track of all the jobs you applied too with this site.
             </p>
             <Link to="/register" className="btn btn-hero">
               Login/Register
